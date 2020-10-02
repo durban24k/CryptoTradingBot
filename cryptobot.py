a4a3bcff810ee4974686239ae63e7931d8b0703e
Wanjau Alfred Mambo <56720300+durban24k@users.noreply.github.com>
2020-10-02 14:56:16 +0300

    v1.0

--- cryptobot.py
+++ cryptobot.py
@@ -1,1 +1,19 @@
-import numpy as np
+import websocket, numpy,json,pprint
+
+
+SOCKET="wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
+SYMBOL="ETHUSDT"
+
+def on_open(ws):
+     print('Connection Open')
+
+def on_close(ws):
+     print("Connection Closed")
+
+def on_message(ws,message):
+     print("Message Received")
+     json_message=json.loads(message)
+     pprint.pprint(json_message)
+
+ws=websocket.WebSocketApp(SOCKET,on_open=on_open,on_close=on_close,on_message=on_message)
+ws.run_forever()
