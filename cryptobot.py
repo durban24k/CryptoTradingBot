d5c87f9a6672e241ae5a600d64ae5c4610105246
Wanjau Alfred Mambo <56720300+durban24k@users.noreply.github.com>
2020-10-02 17:57:06 +0300

    v1.2

--- cryptobot.py
+++ cryptobot.py
@@ -15,5 +15,12 @@ def on_message(ws,message):
      json_message=json.loads(message)
      pprint.pprint(json_message)
 
+     candle=json_message['k']
+     is_candle_closed=candle['x']
+     close=candle['c']
+
+     if is_candle_closed:
+          print(f"Candle closed at {close}".format())
+
 ws=websocket.WebSocketApp(SOCKET,on_open=on_open,on_close=on_close,on_message=on_message)
 ws.run_forever()
